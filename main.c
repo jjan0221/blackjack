1e032750c3e5c520a9459933fa10048c2fbd7cf7
jjan0221 <jjan0221@sookmyung.ac.kr>
2019-11-18 15:15:07 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define N_CARDSET			1
 #define N_CARD				52
@@ -14,6 +15,21 @@
 
 #define N_MIN_ENDCARD		30
 
+//some utility functions
+
+//get an integer input from standard input (keyboard)
+//return : input integer value
+//         (-1 is returned if keyboard input was not integer)
+int getIntegerInput(void) {
+    int input, num;
+    
+    num = scanf("%d", &input);
+    fflush(stdin);
+    if (num != 1) //if it fails to get integer
+        input = -1;
+    
+    return input;
+}
 
 //card tray object
 int CardTray[N_CARDSET*N_CARD];
@@ -35,20 +51,24 @@ int gameEnd = 0; 							//game end flag
 //card processing functions ---------------
 
 //calculate the actual card number in the blackjack game
-int getCardNum(int cardnum) {
+void getCardNum(int CardTray[cardnum]) {
 	if (cardnum%13==0)
+		CardTray[cardnum] = 1;
 	else if (cardnum%13==10)
+		CardTray[cardnum] = 10;
 	else if (cardnum%13==11)
+		CardTray[cardnum] = 10;
 	else if (cardnum%13==12)
+		CardTray[cardnum] = 10;
 	else 
-	
+		CardTray[cardnum] = cardnum%13;
 }
 
 //print the card information (e.g. DiaA)
 void printCard(int cardnum) {
-		if(cardnum/13==0)
+	if(cardnum/13==0)
 	{
-		printf("Spade ");
+		printf("Spade ");	//0<=cardnum<13 ¡æspade A~K 
 		if (cardnum%13==0)
 			printf("A ");
 		else if	(cardnum%13==10)
@@ -62,7 +82,7 @@ void printCard(int cardnum) {
 	}
 	else if(cardnum/13==1)
 	{
-		printf("Heart ");
+		printf("Heart ");	//13<=cardnum<26 ¡æHeart A~K 
 		if (cardnum%13==0)
 			printf("A ");
 		else if	(cardnum%13==10)
@@ -72,9 +92,9 @@ void printCard(int cardnum) {
 		else if (cardnum%13==12)
 			printf("K ");
 		else 
-			printf("%d ",(cardnum%13));
+			printf("%d ",(cardnum%13+1));
 	}
-	else if(cardnum/13==2)
+	else if(cardnum/13==2)	//26<=cardnum<39 ¡æClover A~K 
 	{
 		printf("Clover ");
 		if (cardnum%13==0)
@@ -86,11 +106,11 @@ void printCard(int cardnum) {
 		else if (cardnum%13==12)
 			printf("K ");
 		else 
-			printf("%d ",(cardnum%13));
+			printf("%d ",(cardnum%13+1));
 	}
-	else if(cardnum/13==3)
+	else if(cardnum/13==3)	//39<=cardnum<52 ¡æDia A~K 
 	{
-		printf("Dia");
+		printf("Dia ");
 		if (cardnum%13==0)
 			printf("A ");
 		else if	(cardnum%13==10)
@@ -100,31 +120,37 @@ void printCard(int cardnum) {
 		else if (cardnum%13==12)
 			printf("K ");
 		else 
-			printf("%d ",(cardnum%13));
+			printf("%d ",(cardnum%13+1));
 	}
-	return cardnum;
 }
 
 //card array controllers -------------------------------
 
 //mix the card sets and put in the array
-int mixCardTray() {
-	int i,j, temp; 
+void mixCardTray(int Card[]) {
+	int a, b, i, x; 
 	
 	srand(time(NULL));
 	
-	for(i=0;i<52;i++)
+	printf("card mixed \n");
+	
+	for(i=0;i<100;i++)
 	{
-		j=rand()%52;
-		temp = CardTray[i];
-		CardTray[i] = CardTray[j];
-		CardTray[j] = temp;
+		a = rand % 52;
+		b = rand % 52;
+		x = Card[a];
+		Card[a] = Card[b];
 	}
-
 }
 
 //get one card from the tray
 int pullCard(void) {
+	
+	int i;
+	i = cardIndex;
+	++cardIndex;
+
+	return i;
 }
 
 
@@ -133,10 +159,12 @@ int pullCard(void) {
 //player settiing
 int configUser(void) {
 	int n_user;
-	
-	printf("\n------------------ GAME start --------------------------\n");
-	printf("input the number of players (MAX:5) : ");
-	scanf("%d",&n_user);
+	do
+	{
+		printf("input the number of players (MAX:5) : ");
+		n_user = getIntegerInput();
+	}
+	while (n_user>5&&n_user<1);
 
 	return n_user;
 }
@@ -147,9 +175,9 @@ int betDollar(void) {
 	int betting_money;
 	
 	printf("-------BETTING STEP-------\n");
-	printf("  -> your betting (total:$) : ");
+	printf("  -> your betting (total:$%d) : ");
 	
-	scanf("%d", &betting_money);
+	betting_money = getIntegerInput();
 	
 	return betting_money;
 }
@@ -173,7 +201,26 @@ void offerCards(void) {
 
 //print initial card status
 void printCardInitialStatus(void) {
+	int i;
+	// print server's card
+	printf("server : \t");
+	printf("X \t");
+	printCard(cardhold[n_user][1])	
 	
+	// print my card
+	printf("you : \t");
+	printCard(cardhold[0][0]);
+	printf("\t");
+	printCard(cardhold[0][1]);
+
+	// print players card
+	for(i=1;i<n_user;i++);
+	{
+		printf("player %d : ", i);
+		printCard(cardhold[i][0]);
+		printf("\t");
+		printCard(cardhold[i][1])
+	}
 }
 
 int getAction(void) {
@@ -182,10 +229,10 @@ int getAction(void) {
 	
 	printf("GO?(0) STOP?(other) ");
 	scanf("%d", &x);
-	if(x=0)
-		continue;
+	if(x==0)
+		//continue
 	else
-		break;
+		//stop
 	
 	return x;
 }
@@ -204,7 +251,7 @@ void printUserCardStatus(int user, int cardcnt) {
 
 
 // calculate the card sum and see if : 1. under 21, 2. over 21, 3. blackjack
-int calcStepResult() {
+inget calcStepResult() {
 	
 }
 
@@ -225,12 +272,16 @@ int main(int argc, char *argv[]) {
 	
 	srand((unsigned)time(NULL));
 	
+	printf("\n------------------ GAME start --------------------------\n");
+	
 	//set the number of players
 	n_user = configUser();
 
 
 	//Game initialization --------
 	//1. players' dollar
+	
+	
 	
 	//2. card tray
 	mixCardTray();
@@ -247,7 +298,7 @@ int main(int argc, char *argv[]) {
 	
 		
 		//each player's turn
-		for () //each player
+		for (i=0;i<n_user;i++) //each player
 		{
 			while () //do until the player dies or player says stop
 			{
