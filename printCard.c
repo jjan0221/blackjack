872574072ac1e0de7a6c41fa9d9068a2fbbab1a3
jjan0221 <jjan0221@sookmyung.ac.kr>
2019-11-21 00:07:18 +0900

    Update printCard.c

--- printCard.c
+++ printCard.c
@@ -89,6 +89,64 @@ void printCard(int cardnum) {
 	}
 }
 
+//Function to execute mixCardTray
+void swap(int *px, int *py)
+{
+	int tmp;
+	
+	tmp = *px;
+	*px = *py;
+	*py = tmp;
+}
+
+//mix the card sets and put in the array
+void mixCardTray(void) {
+	int temp;
+	int i, j, a;
+	srand(time(NULL));
+	
+	a = rand()%52;
+	
+	printf("card mixed \n");
+	
+	for(i=0;i<52;i++)
+	{
+		CardTray[i]=i;
+	}
+	
+	for(j=0;j<52;j++)
+	{
+		swap(CardTray[j], CardTray[i]);
+	}
+}
+
+
+//get one card from the tray
+int pullCard(void) {
+	
+	int i;
+	i = cardIndex;
+	cardIndex++;
+	CardTray[i];
+	
+	return CardTray[i];
+}
+
+//offering initial 2 cards
+void offerCards(void) {
+	int i;
+	int n_user;
+	//1. give two card for each players
+	for (i=0;i<n_user;i++)
+	{
+		cardhold[i][0] = pullCard();
+		cardhold[i][1] = pullCard();
+	}
+	//2. give two card for the operator
+	cardhold[n_user][0] = pullCard();
+	cardhold[n_user][1] = pullCard();
+}
+
 //print initial card status
 void printCardInitialStatus(void) {
 	int i;
@@ -113,19 +171,14 @@ void printCardInitialStatus(void) {
 	}
 }
 
-//offering initial 2 cards
-void offerCards(void) {
+void printUserCardStatus(int user, int cardcnt) {
 	int i;
-	int n_user;
-	//1. give two card for each players
-	for (i=0;i<n_user;i++)
-	{
-		cardhold[i][0] = pullCard();
-		cardhold[i][1] = pullCard();
-	}
-	//2. give two card for the operator
-	cardhold[n_user][0] = pullCard();
-	cardhold[n_user][1] = pullCard();
 	
-	return;
+	printf("   -> card : ");
+	for (i=0;i<cardcnt;i++)
+		printCard(cardhold[user][i]);
+		
+	printf("\t ::: ");
 }
+
+void main(void);
